41439aebdf5424b48d9614f1ad51cb1ce472bcd9
Jonathan O'Donnell <bob@gmail.com>
2020-12-04 07:43:26 -0400

    bomb swept

--- main.js
+++ main.js
@@ -1,61 +1,64 @@
-let main = (i)=>{
-  if(i < 100){
-    if(i < 100){
-      if(i < 100){
-        if(i < 100){
-          return 1
-        } else if(i < 50) {
-          return 2
-        }
-      } else if(i < 50) {
-        if(i < 100){
-          return 1
-        } else if(i < 50) {
-          if(i < 100){
-            return 1
-          } else if(i < 50) {
-            if(i < 100){
-              return 1
-            } else if(i < 50) {
-              if(i < 100){
-                return 1
-              } else if(i < 50) {
-                if(i < 100){
-                  return 1
-                } else if(i < 50) {
-                  return 2
-                }
-              }
-            }
-          }
-        }
-      }
-    } else if(i < 50) {
-      return 2
-    }
-  } else if(i < 50) {
-    if(i < 100){
-      return 1
-    } else if(i < 50) {
-      if(i < 100){
-        return 1
-      } else if(i < 50) {
-        if(i < 100){
-          return 1
-        } else if(i < 50) {
-          if(i < 100){
-            return 1
-          } else if(i < 50) {
-            if(i < 100){
-              return 1
-            } else if(i < 50) {
-              return 2
-            }
-          }
-        }
-      }
-    }
-  }
-}
+// let main = (i)=>{
+//   if(i < 100){
+//     if(i < 100){
+//       if(i < 100){
+//         if(i < 100){
+//           return 1
+//         } else if(i < 50) {
+//           return 2
+//         }
+//       } else if(i < 50) {
+//         if(i < 100){
+//           return 1
+//         } else if(i < 50) {
+//           if(i < 100){
+//             return 1
+//           } else if(i < 50) {
+//             if(i < 100){
+//               return 1
+//             } else if(i < 50) {
+//               if(i < 100){
+//                 return 1
+//               } else if(i < 50) {
+//                 if(i < 100){
+//                   return 1
+//                 } else if(i < 50) {
+//                   return 2
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     } else if(i < 50) {
+//       return 2
+//     }
+//   } else if(i < 50) {
+//     if(i < 100){
+//       return 1
+//     } else if(i < 50) {
+//       if(i < 100){
+//         return 1
+//       } else if(i < 50) {
+//         if(i < 100){
+//           return 1
+//         } else if(i < 50) {
+//           if(i < 100){
+//             return 1
+//           } else if(i < 50) {
+//             if(i < 100){
+//               return 1
+//             } else if(i < 50) {
+//               return 2
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// }
 
+let main = (i)=>{
+  return i
+}
 main(3987)
