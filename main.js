bb8a45e042a7f5e7763079266317f35a8a188ccf
Jonathan O'Donnell <joncodo@meri.co>
2020-12-04 07:41:23 -0400

    CC=very very high

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 let main = (i)=>{
   if(i < 100){
     if(i < 100){
-      return 1
+      if(i < 100){
+        if(i < 100){
+          return 1
+        } else if(i < 50) {
+          return 2
+        }
+      } else if(i < 50) {
+        if(i < 100){
+          return 1
+        } else if(i < 50) {
+          if(i < 100){
+            return 1
+          } else if(i < 50) {
+            if(i < 100){
+              return 1
+            } else if(i < 50) {
+              if(i < 100){
+                return 1
+              } else if(i < 50) {
+                if(i < 100){
+                  return 1
+                } else if(i < 50) {
+                  return 2
+                }
+              }
+            }
+          }
+        }
+      }
     } else if(i < 50) {
       return 2
     }
