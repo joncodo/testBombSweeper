fb396f31666a0419e17f6016004f958344bd9e7b
Jonathan O'Donnell <joncodo@meri.co>
2020-12-04 07:38:43 -0400

    CC=8

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ let main = (i)=>{
     if(i < 100){
       return 1
     } else if(i < 50) {
-      return 2
+      if(i < 100){
+        return 1
+      } else if(i < 50) {
+        return 2
+      }
     }
   }
 }
